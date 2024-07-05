da5ca6733fa5a98dbd3064a45086a650c2628b45
cecilialsantin <chikhakituti@gmail.com>
2024-07-05 14:14:50 -0300

    actualizacion de comentarios

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ const port = process.env.PORT || 3030;
 
 app.use(express.json());
 
-// Conectar a la base de datos
+// Conecta la base de datos
 db.authenticate().then(() => {
   console.log('Conectado a la base de datos');
-  db.sync(); // Asegúrate de sincronizar los modelos
+  db.sync(); // Sincroniza los modelos
 }).catch(err => {
   console.error('Error al conectar a la base de datos', err);
 });
@@ -30,40 +30,3 @@ app.use('/ingredientes', ingredienteRoutes);
 app.listen(port, () => {
   console.log(`Servidor en el puerto ${port}`);
 });
-
-/*const express = require("express");
-const recetaRoutes = require('./routes/recetaRoutes');
-const ingredienteRoutes = require('./routes/ingredienteRoutes');
-const cors = require("cors");
-const app = express();
-const port = 3030;
-
-const db = require("./data/db");
-
-app.use(cors()); // Habilita CORS
-app.use(express.json());
-
-app.use('/recetas', recetaRoutes);
-app.use('/ingredientes', ingredienteRoutes);
-
-
-app.get("/", (req, res) => {
-    res.send("Estás en el home");
-});
-
-const conexionDB = async () => {
-    try {
-        await db.authenticate();
-        console.log(`Conectado Ok a la Base de datos`);
-        await db.sync();  // Sincroniza los modelos
-        console.log("Modelos sincronizados");
-    } catch (error) {
-        console.log(`Hay un error y es el siguiente: ${error}`);
-    }
-};
-
-app.listen(port, () => {
-    conexionDB();
-    console.log(`Servidor Ok en el puerto ${port}`);
-});*/ 
-
--- routes/recetaRoutes.js
+++ routes/recetaRoutes.js
@@ -13,15 +13,3 @@ router.get('/', recetaController.getRecetas);
 
 module.exports = router;
 
-
-/*const express = require('express');
-const recetaController = require('../controllers/recetaController');
-
-const router = express.Router();
-
-router.post('/', recetaController.createReceta);
-router.get('/', recetaController.getRecetas);
-router.put('/:id', recetaController.updateReceta);
-router.delete('/:id', recetaController.deleteReceta);
-
-module.exports = router;*/
